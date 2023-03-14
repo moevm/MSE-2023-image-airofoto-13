89de3e6ee75022a1619b4fded62d116335dce32e
oksi-oksik <oksi-oksik13@mail.ru>
2023-03-14 17:53:38 +0300

    add tests for checking rotation

---
+++ tests/test_rotation.py
@@ -0,0 +1,59 @@
+import numpy as np
+import open3d as o3d  # type: ignore
+from rotation.rotation import rotate_file
+import math
+
+
+# checking the rotation for an angle in radians
+def test_rotate_ply_file_rotates_radians():
+    ply_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/model.ply")
+    ply_np: np.ndarray = np.asarray(ply_file.points)
+    expected_result_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/rotate_z_90.ply")
+    expected_result: np.ndarray = np.asarray(expected_result_file.points)
+    rotated_cloud: o3d.cpu.pybind.geometry.PointCloud = rotate_file(ply_np, "Radian", 0, 0, math.pi / 2)
+    rotated_np: np.ndarray = np.asarray(rotated_cloud.points)
+    assert np.array_equal(np.round(rotated_np, 2), np.round(expected_result, 2))
+
+
+# checking the rotation at an angle of 90 degrees along the z axis
+def test_rotate_ply_file_rotates_z_90():
+    ply_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/model.ply")
+    ply_np: np.ndarray = np.asarray(ply_file.points)
+    expected_result_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/rotate_z_90.ply")
+    expected_result: np.ndarray = np.asarray(expected_result_file.points)
+    rotated_cloud: o3d.cpu.pybind.geometry.PointCloud = rotate_file(ply_np, "Degree", 0, 0, 90)
+    rotated_np: np.ndarray = np.asarray(rotated_cloud.points)
+    assert np.array_equal(np.round(rotated_np, 2), np.round(expected_result, 2))
+
+
+# checking the rotation at an angle of 30 degrees along the x axis
+def test_rotate_ply_file_rotates_x_30():
+    ply_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/model.ply")
+    ply_np: np.ndarray = np.asarray(ply_file.points)
+    expected_result_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/rotate_x_30.ply")
+    expected_result: np.ndarray = np.asarray(expected_result_file.points)
+    rotated_cloud: o3d.cpu.pybind.geometry.PointCloud = rotate_file(ply_np, "Degree", 30, 0, 0)
+    rotated_np: np.ndarray = np.asarray(rotated_cloud.points)
+    assert np.array_equal(np.round(rotated_np, 2), np.round(expected_result, 2))
+
+
+# checking rotation along multiple axes
+def test_rotate_ply_file_rotates_x_30_z_90():
+    ply_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/model.ply")
+    ply_np: np.ndarray = np.asarray(ply_file.points)
+    expected_result_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/rotate_x_30_z_90.ply")
+    expected_result: np.ndarray = np.asarray(expected_result_file.points)
+    rotated_cloud: o3d.cpu.pybind.geometry.PointCloud = rotate_file(ply_np, "Degree", 30, 0, 90)
+    rotated_np: np.ndarray = np.asarray(rotated_cloud.points)
+    assert np.array_equal(np.round(rotated_np, 2), np.round(expected_result, 2))
+
+
+# checking rotation at an angle of 270.5 degrees along the y axis
+def test_rotate_ply_file_rotates_y_270_5():
+    ply_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/model.ply")
+    ply_np: np.ndarray = np.asarray(ply_file.points)
+    expected_result_file: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud("../images/rotate_y_270_5.ply")
+    expected_result: np.ndarray = np.asarray(expected_result_file.points)
+    rotated_cloud: o3d.cpu.pybind.geometry.PointCloud = rotate_file(ply_np, "Degree", 0, 270.5, 0)
+    rotated_np: np.ndarray = np.asarray(rotated_cloud.points)
+    assert np.array_equal(np.round(rotated_np), np.round(expected_result))
