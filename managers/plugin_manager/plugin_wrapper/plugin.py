c0b94f1813cf12cd6fb26619e89e774c241f8298
N3xTr1Me <destriker17@gmail.com>
2023-04-01 21:53:50 +0300

    Added plugin wrapper class and dedicated Factory

---
+++ managers/plugin_manager/plugin_wrapper/plugin.py
@@ -0,0 +1,20 @@
+from inspect import signature, Signature
+from typing import Any, List, Dict
+
+from framework import ICommand, ITarget
+from .plugin_base import IPlugin
+
+
+class Plugin(ICommand, IPlugin):
+
+    def __init__(self, executable: callable) -> None:
+        self.__executable = executable
+
+    def get_signature(self) -> Signature:
+        return signature(self.__executable)
+
+    def get_help(self) -> str:
+        return self.__executable.__doc__
+
+    def execute(self, target: ITarget, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+        target.receive(self.__executable(*args, **kwargs))
---
+++ managers/plugin_manager/plugin_wrapper/plugin_base.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from inspect import Signature
+from typing import Optional
+
+
+class IPlugin(ABC):
+
+    """
+    A generic wrapper for plug-in functions
+    """
+
+    @abstractmethod
+    def get_signature(self) -> Signature:
+        """
+        Method to acquire signature of the function wrapped by the IPlugin instance.
+        Required for internal business logic.
+
+        :return: Signature: Signature object from inspect module, describing the wrapped function.
+        """
+
+        pass
+
+    @abstractmethod
+    def get_help(self) -> str:
+        """
+        Method to get a help-message about the function wrapped by the IPlugin instance.
+        Required for internal business logic.
+
+        :return: str: help message, usually the __doc__ attribute of the function.
+        """
+        pass
+
+
+class IPluginFactory(ABC):
+
+    """
+    A Factory pattern class designated to serialization of IPlugin instances.
+    """
+
+    @staticmethod
+    @abstractmethod
+    def make_plugin(name: str, package: Optional[str] = None) -> IPlugin:
+        """
+        Static method for simple IPlugin instance creation.
+
+        :param name: string containing the exact name of the module AND target function in the module to wrap.
+        :param package: [Optional] name of the package to import from.
+        If not provided the standard __plugin_directory will be used.
+        :return: IPlugin: instance of the IPlugin wrapper class.
+        """
+
+        pass
