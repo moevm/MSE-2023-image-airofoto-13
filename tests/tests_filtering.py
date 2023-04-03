24d4789ada7c02d00837a04d6cd003641beb07f9
oksi-oksik <oksi-oksik@users.noreply.github.com>
2023-04-03 19:39:17 +0000

    Format code with black

--- tests/tests_filtering.py
+++ tests/tests_filtering.py
@@ -10,9 +10,11 @@ def test_filtering_1() -> None:
         "../images/model.ply"
     )
     ply_np: NDArray[np.float_] = np.asarray(ply_file.points)
-    filter_cloud: o3d.cpu.pybind.geometry.PointCloud = filtering.filter_point_cloud_by_height(
-        ply_np, np.asarray(ply_file.colors), -3, -1.5
-    )[0]
+    filter_cloud: o3d.cpu.pybind.geometry.PointCloud = (
+        filtering.filter_point_cloud_by_height(
+            ply_np, np.asarray(ply_file.colors), -3, -1.5
+        )[0]
+    )
     filter_cloud_np: NDArray[np.float_] = np.asarray(filter_cloud.points)
     assert len(filter_cloud_np) == sum((-3 <= i <= -1.5) for i in ply_np[:, 2])
 
@@ -23,9 +25,11 @@ def test_filtering_2() -> None:
         "../images/model.ply"
     )
     ply_np: NDArray[np.float_] = np.asarray(ply_file.points)
-    filter_cloud_2: o3d.cpu.pybind.geometry.PointCloud = filtering.filter_point_cloud_by_height(
-        ply_np, np.asarray(ply_file.colors), 1, 3
-    )[0]
+    filter_cloud_2: o3d.cpu.pybind.geometry.PointCloud = (
+        filtering.filter_point_cloud_by_height(
+            ply_np, np.asarray(ply_file.colors), 1, 3
+        )[0]
+    )
     filter_cloud_np_2: NDArray[np.float_] = np.asarray(filter_cloud_2.points)
     assert len(filter_cloud_np_2) == 0
 
@@ -36,8 +40,10 @@ def test_filtering_3() -> None:
         "../images/model.ply"
     )
     ply_np: NDArray[np.float_] = np.asarray(ply_file.points)
-    filter_cloud_3: o3d.cpu.pybind.geometry.PointCloud = filtering.filter_point_cloud_by_height(
-        ply_np, np.asarray(ply_file.colors), -100, 100
-    )[0]
+    filter_cloud_3: o3d.cpu.pybind.geometry.PointCloud = (
+        filtering.filter_point_cloud_by_height(
+            ply_np, np.asarray(ply_file.colors), -100, 100
+        )[0]
+    )
     filter_cloud_np_3: NDArray[np.float_] = np.asarray(filter_cloud_3.points)
     assert np.array_equal(ply_np, filter_cloud_np_3)
