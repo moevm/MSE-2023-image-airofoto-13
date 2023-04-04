49d8bb4e08f9a0ae3e8d58ef426e6601c8251d19
N3xTr1Me <destriker17@gmail.com>
2023-04-04 22:57:12 +0300

    Model class added

---
+++ mvp/model/model.py
@@ -0,0 +1,42 @@
+from typing import List, Any, Dict, Optional
+from os.path import join
+import open3d # type: ignore
+
+from .model_base import IModel
+from managers import PluginRegistry, FileManager, PluginInfo
+
+
+class Model(IModel):
+
+    def __init__(self, registry: Optional[PluginRegistry] = None):
+
+        if registry is None:
+            self.__operations = PluginRegistry()
+            self.__operations.add_plugin("rotate")
+        else:
+            self.__operations = registry
+
+        self.__operations.set_target(self)
+
+        self.__output_name = join(".", "output.ply")
+
+        self.__data : open3d.geometry.PointCloud | None = None
+
+    def receive(self, result: open3d.geometry.PointCloud) -> None:
+        self.__data = result
+
+    def load_data(self, path: str) -> None:
+        self.__data = FileManager().read(path)
+
+    def save_data(self, path: str) -> None:
+        if self.__data is not None:
+            FileManager().write(self.__output_name, self.__data)
+
+    def task(self, operation: str, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+        self.__operations.invoke(operation, *args, **kwargs)
+
+    def operation_info(self, name: str) -> PluginInfo:
+        return self.__operations.get_info(name)
+
+    def supported_operations(self) -> List[str]:
+        return self.__operations.supported_plugins()
---
+++ mvp/model/model_base.py
@@ -0,0 +1,78 @@
+from abc import abstractmethod
+from typing import Dict, Any, Tuple, List
+
+from framework import ITarget, EmbedSingleton
+from managers import PluginInfo
+
+
+class IModel(ITarget, metaclass=EmbedSingleton):
+
+    """
+    Interface for Model object from MVP pattern. Defines the essential data management and processing logic.
+    """
+
+
+    @abstractmethod
+    def load_data(self, path: str) -> None:
+        """
+        Loads the PointCloud data from file along the given path. Stores internally.
+
+        :param path: path to the raw data file.
+        :return: None
+        """
+
+        pass
+
+    @abstractmethod
+    def save_data(self, path: str) -> None:
+        """
+        Saves the current PointCloud data stored in IModel instance to the file along the given path.
+
+        :param path: path to save file to.
+        :return: None
+        """
+
+        pass
+
+    @abstractmethod
+    def task(self, operation: str, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+        """
+        Perform a single task of processing the data stored.
+        Updates the data stored with the result of the performed operation.
+
+        :raises: RuntimeError: if the data was not loaded to IModel instance prior to the task() invocation.
+
+        :param operation: name/type of the operation to perform on data.
+        :param args: positional arguments.
+        :param kwargs: key-value arguments.
+        :return: None
+        """
+
+        pass
+
+    # @abstractmethod
+    # def execute_tasks(self, tasks: Dict[str, Dict[str, Any]]) -> None:
+    #     pass
+
+    @abstractmethod
+    def operation_info(self, name: str) -> PluginInfo:
+        """
+        Returns the PluginInfo instance with the information about the desired operation.
+
+        :raises: KeyError: if the desired operation is not supported.
+
+        :param name: name/type of the operation.
+        :return: PluginInfo
+        """
+
+        pass
+
+    @abstractmethod
+    def supported_operations(self) -> List[str]:
+        """
+        Returns a list of supported operations.
+
+        :return: List[str]: list of supported operations.
+        """
+
+        pass
