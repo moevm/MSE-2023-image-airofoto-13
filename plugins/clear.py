bd1341cac85806200751b20accf0bfcaa5b07de6
galinaOkhotnikova <galinaOkhotnikova@users.noreply.github.com>
2023-04-24 17:37:50 +0000

    Commit from GitHub Actions

--- plugins/clear.py
+++ plugins/clear.py
@@ -13,20 +13,21 @@ def clear(
     pc_np: NDArray[float_] = np.asarray(data.points)
     np_colors: NDArray[float_] = np.asarray(data.colors)
 
-    if (above == below):
-        raise Exception('Модель не будет изменена при равных значениях параметров above и below')
+    if above == below:
+        raise Exception(
+            "Модель не будет изменена при равных значениях параметров above и below"
+        )
 
-    if (above):
+    if above:
         mask: list[bool] = np.where(pc_np[:, 2] <= height, True, False)
 
-    elif (below):
-        mask: list[bool] = np.where(pc_np[:, 2] >= height, True, False)  
+    elif below:
+        mask: list[bool] = np.where(pc_np[:, 2] >= height, True, False)
 
     result_cloud: o3d.geometry.PointCloud = o3d.geometry.PointCloud()
     result_cloud.points = o3d.utility.Vector3dVector(pc_np[mask])
-    result_cloud.colors = o3d.utility.Vector3dVector(np_colors[mask])  
+    result_cloud.colors = o3d.utility.Vector3dVector(np_colors[mask])
     result_cloud.estimate_covariances()
     result_cloud.estimate_normals()
 
     return result_cloud
-
