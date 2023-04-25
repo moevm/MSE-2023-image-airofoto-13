52d8a0718190ae4ec8378c980aa25b26d0c8ad29
N3xTr1Me <destriker17@gmail.com>
2023-04-25 22:16:32 +0300

    Argument constraints added

--- framework/__init__.py
+++ framework/__init__.py
@@ -1,5 +1,15 @@
 from .abstract_factory import IFactory
 from .singleton import EmbedSingleton
 from .command import ICommand, ITarget
+from .limit import Limit, ChoiceConstraint, PathConstraint
+from .loader import ILoader, Loader
 
-__all__ = ["IFactory", "EmbedSingleton", "ICommand", "ITarget"]
+
+__all__ = ["IFactory",
+           "EmbedSingleton",
+           "ICommand",
+           "ITarget",
+           "Loader",
+           "Limit",
+           "ChoiceConstraint",
+           "PathConstraint"]
