9f03f9013c824a39fc6bab6a7bf681b4bfa6ef9b
Ekaterina0303 <71796000+Ekaterina0303@users.noreply.github.com>
2023-04-25 10:23:56 +0300

    fix types

--- cropping_at_the_edges/cropping_by_cycle.py
+++ cropping_at_the_edges/cropping_by_cycle.py
@@ -1,17 +1,18 @@
 import numpy as np
 import open3d as o3d
-from numpy import ndarray, dtype
-from typing import Any, Tuple, List
+from numpy.typing import NDArray
+from numpy import float_
+from typing import Any, Tuple
 
 
-def crop_by_cycle(pc: o3d.cpu.pybind.geometry.PointCloud, crop_rx: float, crop_lx: float, crop_fy: float, crop_by: float) -> Tuple[Any, List[Any]]:
+def crop_by_cycle(pc: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, crop_fy: float, crop_by: float) -> o3d.geometry.PointCloud:
     result: list[Any] = []
     colors: list[Any] = []
-    point_cloud: ndarray = np.asarray(pc.points)
-    colors_pc: ndarray = np.asarray(pc.colors)
+    point_cloud: NDArray[float_] = np.asarray(pc.points)
+    colors_pc: NDArray[float_] = np.asarray(pc.colors)
 
     for point, color in zip(point_cloud, colors_pc):
-        if crop_lx <= abs(np.array(point)[0]) <= crop_rx and crop_by <= abs(np.array(point)[1]) <= crop_fy:
+        if crop_lx <= np.array(point)[0] <= crop_rx and crop_by <= np.array(point)[1] <= crop_fy:
             new_point = np.array(point)
             colors.append(color)
             result.append(new_point)
@@ -27,11 +28,10 @@ def crop_by_cycle(pc: o3d.cpu.pybind.geometry.PointCloud, crop_rx: float, crop_l
 
 if __name__ == "__main__":
     input_file: str = input()
-    pc: o3d.cpu.pybind.geometry.PointCloud = o3d.io.read_point_cloud(input_file)
-    new_pc: Tuple[Any, List[Any]] = crop_by_cycle(pc, 30, 1, 50, 1)
+    pc: o3d.geometry.PointCloud = o3d.io.read_point_cloud(input_file)
+    new_pc: Tuple[o3d.geometry.PointCloud, NDArray[float_]] = crop_by_cycle(pc, 30, 1, 50, 1)
     new_file = new_pc[0]
     new_colors = new_pc[1]
     new_file.colors = o3d.utility.Vector3dVector(new_colors)
     o3d.io.write_point_cloud("new_file.ply", new_file)
     o3d.visualization.draw_geometries([new_file])
-    
