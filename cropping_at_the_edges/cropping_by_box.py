e35e9e8d236b69cbc71f2823a928b80a343ed28e
Ekaterina0303 <Ekaterina0303@users.noreply.github.com>
2023-05-05 18:59:36 +0000

    Commit from GitHub Actions

--- cropping_at_the_edges/cropping_by_box.py
+++ cropping_at_the_edges/cropping_by_box.py
@@ -1,19 +1,26 @@
 import numpy as np
-import open3d as o3d # type: ignore
+import open3d as o3d  # type: ignore
 from numpy.typing import NDArray
 from numpy import float_
 import math
 import itertools
 from typing import Any, Tuple
 
-def crop_by_box(pc: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, crop_fy: float, crop_by: float) -> o3d.geometry.PointCloud:
+
+def crop_by_box(
+    pc: o3d.geometry.PointCloud,
+    crop_rx: float,
+    crop_lx: float,
+    crop_fy: float,
+    crop_by: float,
+) -> o3d.geometry.PointCloud:
 
     box: o3d.geometry.AxisAlignedBoundingBox = pc.get_axis_aligned_bounding_box()
     box_points: o3d.utility.Vector3dVector = box.get_box_points()
     coord: NDArray[float_] = np.asarray(box_points)
 
     arr: list[NDArray[float_]] = []
-    for x in np.nditer(coord, flags=['external_loop'], order='F'):
+    for x in np.nditer(coord, flags=["external_loop"], order="F"):
         arr.append(x)
     left_x: float = min(arr[0])
     right_x: float = max(arr[0])
@@ -25,9 +32,15 @@ def crop_by_box(pc: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, cro
     back_y += crop_by
     front_y -= crop_fy
 
-    bounds: list[list[float]] = [[left_x, right_x], [back_y, front_y], [-math.inf, math.inf]]
+    bounds: list[list[float]] = [
+        [left_x, right_x],
+        [back_y, front_y],
+        [-math.inf, math.inf],
+    ]
     bounding_box_points: list[Tuple[Any]] = list(itertools.product(*bounds))
-    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(o3d.utility.Vector3dVector(bounding_box_points))
+    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
+        o3d.utility.Vector3dVector(bounding_box_points)
+    )
     pc_croped: o3d.geometry.PointCloud = pc.crop(bounding_box)
     return pc_croped
 
