06166529a4a0d4a4b0b7239b125ddf9b7443cfc6
Ekaterina0303 <Ekaterina0303@users.noreply.github.com>
2023-05-08 19:21:02 +0000

    Commit from GitHub Actions

--- sewing_the_holes/sewing_the_holes.py
+++ sewing_the_holes/sewing_the_holes.py
@@ -1,19 +1,25 @@
-import open3d as o3d # type: ignore
+import open3d as o3d  # type: ignore
 import numpy as np
 from numpy.typing import NDArray
 from numpy import float_
 
 
-def sewing_the_holes(pc: o3d.geometry.PointCloud, radius: float) -> o3d.geometry.PointCloud:
+def sewing_the_holes(
+    pc: o3d.geometry.PointCloud, radius: float
+) -> o3d.geometry.PointCloud:
     pc.estimate_normals()
     distances: open3d.utility.DoubleVector = pc.compute_nearest_neighbor_distance()
     avg_dist: NDArray[float_] = np.mean(distances)
     radius: float = 1.5 * avg_dist
-    mesh: open3d.geometry.TriangleMesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pc, o3d.utility.DoubleVector([radius, radius * 2]))
-    filled = o3d.t.geometry.TriangleMesh.from_legacy(mesh).fill_holes(radius).to_legacy()
-    result_cloud: o3d.geometry.PointCloud = (
-        o3d.geometry.PointCloud()
+    mesh: open3d.geometry.TriangleMesh = (
+        o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+            pc, o3d.utility.DoubleVector([radius, radius * 2])
+        )
     )
+    filled = (
+        o3d.t.geometry.TriangleMesh.from_legacy(mesh).fill_holes(radius).to_legacy()
+    )
+    result_cloud: o3d.geometry.PointCloud = o3d.geometry.PointCloud()
     result_cloud.points = filled.vertices
     result_cloud.colors = filled.vertex_colors
     result_cloud.normals = filled.vertex_normals
