e5ef0f541f64878ffd19133feaa75caf86751452
Ekaterina0303 <71796000+Ekaterina0303@users.noreply.github.com>
2023-05-16 11:24:21 +0300

    update

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,8 @@
 from .clear import clear
-from .cut import cut
+from .crop_by_cycle import crop_by_cycle
+from .crop_by_box import crop_by_box
 from .mount import mount
 from .patch import patch
 from .rotate import rotate
 
-__all__ = ["clear", "cut", "mount", "patch", "rotate"]
+__all__ = ["clear", "crop_by_cycle", "crop_by_box", "mount", "patch", "rotate"]
--- plugins/crop_by_box.py
+++ plugins/crop_by_box.py
@@ -1,35 +1,28 @@
 import numpy as np
-import open3d as o3d  # type: ignore
+import open3d as o3d # type: ignore
 from numpy.typing import NDArray
 from numpy import float_
 import math
 import itertools
-from typing import Any, Tuple
+from typing import Any, Tuple, List
 
-
-def crop_by_box(
-    pc: o3d.geometry.PointCloud,
-    crop_rx: float,
-    crop_lx: float,
-    crop_fy: float,
-    crop_by: float,
-) -> o3d.geometry.PointCloud:
+def crop_by_box(pc: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, crop_fy: float, crop_by: float) -> o3d.geometry.PointCloud:
     """
-    Dummy function for cut transformation of PointCloud
+        Dummy function for cut transformation of PointCloud
 
-    :param data: PointCloud to transform.
-    :param ul: float flag for cutting the upper-left corner.
-    :param ur: float flag for cutting the upper-right corner.
-    :param ll: float flag for cutting the lower-left corner.
-    :param lr: float flag for cutting the lower-right corner.
-    :return: PointCloud
-    """
+        :param data: PointCloud to transform.
+        :param ul: float flag for cutting the upper-left corner.
+        :param ur: float flag for cutting the upper-right corner.
+        :param ll: float flag for cutting the lower-left corner.
+        :param lr: float flag for cutting the lower-right corner.
+        :return: PointCloud
+        """
     box: o3d.geometry.AxisAlignedBoundingBox = pc.get_axis_aligned_bounding_box()
     box_points: o3d.utility.Vector3dVector = box.get_box_points()
     coord: NDArray[float_] = np.asarray(box_points)
 
-    arr: list[NDArray[float_]] = []
-    for x in np.nditer(coord, flags=["external_loop"], order="F"):
+    arr: List[NDArray[float_]] = []
+    for x in np.nditer(coord, flags=['external_loop'], order='F'):
         arr.append(x)
     left_x: float = min(arr[0])
     right_x: float = max(arr[0])
@@ -49,14 +42,9 @@ def crop_by_box(
     if back_y > front_y0 or front_y < back_y0:
         back_y = front_y = 0
 
-    bounds: list[list[float]] = [
-        [left_x, right_x],
-        [back_y, front_y],
-        [-math.inf, math.inf],
-    ]
-    bounding_box_points: list[Tuple[Any]] = list(itertools.product(*bounds))
-    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
-        o3d.utility.Vector3dVector(bounding_box_points)
-    )
+    bounds: list[list[float]] = [[left_x, right_x], [back_y, front_y], [-math.inf, math.inf]]
+    bounding_box_points: List[Tuple[Any]] = list(itertools.product(*bounds))
+    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(o3d.utility.Vector3dVector(bounding_box_points))
     pc_cropped: o3d.geometry.PointCloud = pc.crop(bounding_box)
     return pc_cropped
+
