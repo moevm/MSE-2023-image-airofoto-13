ae2d26611d943688933bbae3ae602798596dfcfa
Ekaterina0303 <Ekaterina0303@users.noreply.github.com>
2023-06-01 16:51:49 +0000

    Commit from GitHub Actions

--- plugins/mount.py
+++ plugins/mount.py
@@ -1,5 +1,5 @@
 import open3d as o3d  # type: ignore
-from osgeo import gdal # type: ignore
+from osgeo import gdal  # type: ignore
 import numpy as np
 
 
@@ -21,12 +21,16 @@ def mount(data: o3d.geometry.PointCloud, picture_path: str) -> o3d.geometry.Poin
     origin_x = geotransform[0]
     origin_y = geotransform[3]
 
-    geotiff_data_rgb = np.stack([geotiff_data]*3, axis=-1)
+    geotiff_data_rgb = np.stack([geotiff_data] * 3, axis=-1)
 
     image = o3d.geometry.Image(geotiff_data_rgb)
     image_data = o3d.geometry.imageData()
     image_data.set_data(image)
-    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_data, data, depth_scale = 1.0, convert_rgb_to_intensity=False)
-    cloud_result = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        image_data, data, depth_scale=1.0, convert_rgb_to_intensity=False
+    )
+    cloud_result = o3d.geometry.PointCloud.create_from_rgbd_image(
+        rgbd_image, o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
+    )
 
     return cloud_result
