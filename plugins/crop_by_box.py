bdefd0d5328b44d8551b132f4ec3f983084fb676
Ekaterina0303 <Ekaterina0303@users.noreply.github.com>
2023-06-01 18:18:54 +0000

    Commit from GitHub Actions

--- plugins/crop_by_box.py
+++ plugins/crop_by_box.py
@@ -1,28 +1,35 @@
 import numpy as np
-import open3d as o3d # type: ignore
+import open3d as o3d  # type: ignore
 from numpy.typing import NDArray
 from numpy import float_
 import math
 import itertools
 from typing import Any, Tuple, List
 
-def crop_by_box(data: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, crop_fy: float, crop_by: float) -> o3d.geometry.PointCloud:
-    """
-        Dummy function for cut transformation of PointCloud
 
-        :param data: PointCloud to transform.
-        :param crop_rx: float flag for cutting the right x coordinate.
-        :param crop_lx: float flag for cutting the right x coordinate.
-        :param crop_fy: float flag for cutting front y coordinate.
-        :param crop_by: float flag for cutting back y coordinate.
-        :return: PointCloud
-        """
+def crop_by_box(
+    data: o3d.geometry.PointCloud,
+    crop_rx: float,
+    crop_lx: float,
+    crop_fy: float,
+    crop_by: float,
+) -> o3d.geometry.PointCloud:
+    """
+    Dummy function for cut transformation of PointCloud
+
+    :param data: PointCloud to transform.
+    :param crop_rx: float flag for cutting the right x coordinate.
+    :param crop_lx: float flag for cutting the right x coordinate.
+    :param crop_fy: float flag for cutting front y coordinate.
+    :param crop_by: float flag for cutting back y coordinate.
+    :return: PointCloud
+    """
     box: o3d.geometry.AxisAlignedBoundingBox = data.get_axis_aligned_bounding_box()
     box_points: o3d.utility.Vector3dVector = box.get_box_points()
     coord: NDArray[float_] = np.asarray(box_points)
 
     arr: list[NDArray[float_]] = []
-    for x in np.nditer(coord, flags=['external_loop'], order='F'):
+    for x in np.nditer(coord, flags=["external_loop"], order="F"):
         arr.append(x)
     left_x: float = min(arr[0])
     right_x: float = max(arr[0])
@@ -42,9 +49,14 @@ def crop_by_box(data: o3d.geometry.PointCloud, crop_rx: float, crop_lx: float, c
     if back_y > front_y0 or front_y < back_y0:
         back_y = front_y = 0
 
-    bounds: list[list[float]] = [[left_x, right_x], [back_y, front_y], [-math.inf, math.inf]]
+    bounds: list[list[float]] = [
+        [left_x, right_x],
+        [back_y, front_y],
+        [-math.inf, math.inf],
+    ]
     bounding_box_points: list[Tuple[Any]] = list(itertools.product(*bounds))
-    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(o3d.utility.Vector3dVector(bounding_box_points))
+    bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
+        o3d.utility.Vector3dVector(bounding_box_points)
+    )
     pc_cropped: o3d.geometry.PointCloud = data.crop(bounding_box)
     return pc_cropped
-
--- plugins/mount.py
+++ plugins/mount.py
@@ -1,5 +1,5 @@
 import open3d as o3d  # type: ignore
-from osgeo import gdal # type: ignore
+from osgeo import gdal  # type: ignore
 import numpy as np
 from numpy.typing import NDArray
 from numpy import float_
@@ -16,12 +16,20 @@ def mount(data: o3d.geometry.PointCloud, picture_path: str) -> o3d.geometry.Poin
     geotiff_dataset: gdal.Dataset = gdal.Open(picture_path)
     geotiff_data: NDArray[float_] = geotiff_dataset.GetRasterBand(1).ReadAsArray()
 
-    geotiff_data_rgb:  NDArray[float_] = np.stack([geotiff_data]*3, axis=-1)
+    geotiff_data_rgb: NDArray[float_] = np.stack([geotiff_data] * 3, axis=-1)
 
     image: o3d.geometry.Image = o3d.geometry.Image(geotiff_data_rgb)
     image_data: o3d.geometry.imageData = o3d.geometry.imageData()
     image_data.set_data(image)
-    rgbd_image: o3d.geometry.RGBDImage = o3d.geometry.RGBDImage.create_from_color_and_depth(image_data, data, depth_scale = 1.0, convert_rgb_to_intensity=False)
-    cloud_result: o3d.geometry.PointCloud = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+    rgbd_image: o3d.geometry.RGBDImage = (
+        o3d.geometry.RGBDImage.create_from_color_and_depth(
+            image_data, data, depth_scale=1.0, convert_rgb_to_intensity=False
+        )
+    )
+    cloud_result: o3d.geometry.PointCloud = (
+        o3d.geometry.PointCloud.create_from_rgbd_image(
+            rgbd_image, o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
+        )
+    )
 
     return cloud_result
